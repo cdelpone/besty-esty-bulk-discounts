0775b2daf3e70af64d60bde0332632431cb52e37
Christina Delpone <cdelpone@gmail.com>
2021-09-25 14:31:02 -0600

    Create bulk discounts relationship to merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -9,6 +9,7 @@ class Merchant < ApplicationRecord
   has_many :invoices, through: :invoice_items
   has_many :transactions, through: :invoices
   has_many :customers, through: :invoices
+  has_many :bulk_discounts
 
   scope :by_status, lambda { |status|
     where(status: status)
