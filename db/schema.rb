98169f2206dffb2bbeea77a16864c4c842c9d4eb
Christina Delpone <cdelpone@gmail.com>
2021-09-28 21:34:14 -0600

    Change percentage from decimal to integer

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2021_09_28_164133) do
 
   create_table "bulk_discounts", force: :cascade do |t|
     t.integer "threshold"
-    t.decimal "percentage"
+    t.integer "percentage"
     t.bigint "merchant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
