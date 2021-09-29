6f7d7aaf9f04d3a73ec14b8bc9df69cc97b52311
Christina Delpone <cdelpone@gmail.com>
2021-09-29 00:59:37 -0600

    remove unnecessary relationships

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,5 +1,6 @@
 class BulkDiscount < ApplicationRecord
   belongs_to :merchant
+
   validates :threshold,
             numericality: { only_integer: true,
                             greater_than_or_equal_to: 1 }
